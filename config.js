021a6f35360385a4fd6fed3e993e01661174cf31
liqthr <tony3rd@outlook.com.au>
2025-10-07 10:20:32 +0000

    Fixed platform-logo styling.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export default {
     spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
     spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
     soundCloudClientId: process.env.SOUNDCLOUD_CLIENT_ID,
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'https://music-bot-brown-nine.vercel.app/'
   };
   
   export function switchMode(mode) {
